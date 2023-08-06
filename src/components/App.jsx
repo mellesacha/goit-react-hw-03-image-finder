758caba36a73372cdc86612b06440b65d5de2eb2
mellesacha <123104912+mellesacha@users.noreply.github.com>
2023-08-06 13:35:57 +0400

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,9 @@ import { Component } from "react";
 import Searchbar from "./Searchbar";
 import ImageGallery from "./ImageGallery";
 import Button from "./Button";
+import Loader from "./Loader";
 import fetchApi from "../service/Fetchapi";
+
 
 export class App extends Component {
 
@@ -10,10 +12,12 @@ export class App extends Component {
     search: "",
     picture: '',
     page: 1,
+    loadBtn: false,
+    loader: false
   }
 
   handleSubmit = (query) => {
-    this.setState({ search: query });
+    this.setState({ search: query, loader: true });
 
     console.log(this.state.search)
     
@@ -27,30 +31,24 @@ export class App extends Component {
      
     })
   }
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(_, prevState) {
     
-    const { search, page } = this.state;
-    const prevPage = prevState.page;
-    const prevSearch = prevState.search;
+    const { search, page} = this.state;
 
-    console.log(search)
-    console.log(prevSearch);
-
-
-    if (search !== prevSearch || page !== prevPage) {
-        fetchApi(search, page).
-          then(r => this.setState(ps => { return { picture: [...ps.picture, ...r.hits] } }));
+    if (search !== prevState.search || page !== prevState.page) {
+       fetchApi(search, page).then(r => this.setState(ps => { return { picture: [...ps.picture, ...r.hits] } }))
+      
     }
-    
-   
-    }
+}
   
-
   render() {
     return (<>
       <Searchbar onSubmit={this.handleSubmit}  />
       <ImageGallery picture={this.state.picture} />
-      <Button handleBtnClick={this.handleBtnClick } />
+      {this.state.picture && <Button handleBtnClick={this.handleBtnClick} />}
+      {this.state.loader && <Loader />}
+      
+      
     </>
     
   );
--- src/service/Fetchapi.js
+++ src/service/Fetchapi.js
@@ -1,6 +1,8 @@
 const BASE_URL = "https://pixabay.com/api/";
 const KEY = "37603150-cf2f7acce1783634ed18c2efe";
 
+const data = '';
+
 
 const fetchApi = (search, page) => {
     return fetch(`${BASE_URL}?q=${search}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`).
